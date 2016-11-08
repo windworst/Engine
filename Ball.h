1b0199f9d3f1caf2a7328aa56d343bf9f9fe416e
anonymous <windworst@gmail.com>
2016-11-09 01:15:55 +0800

    fixbug: collision

--- Ball.h
+++ Ball.h
@@ -32,7 +32,7 @@ class Ball: public Engine::Atom {
     }
 
     Engine::Vector collision(const Engine::Rect& rect) {
-      if(!mRect.collision(rect)) return Engine::Vector();
+      if(!this->rect().collision(rect)) return Engine::Vector();
       Engine::Vector vector = rect.toNearPoint(position());
       if(vector.x() * vector.x() + vector.y() * vector.y() > r()*r()) {
         vector.set(0,0);
